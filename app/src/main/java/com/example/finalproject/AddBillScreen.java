dd23b3737f7ca99cd66c983a67530d45445dbd8a
Yev Albano <94223536+yevgeny182@users.noreply.github.com>
2024-11-26 18:23:19 +0800

    added bill screen form without backend

--- app/src/main/java/com/example/finalproject/AddBillScreen.java
+++ app/src/main/java/com/example/finalproject/AddBillScreen.java
@@ -1,8 +1,11 @@
 package com.example.finalproject;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -11,8 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class AddBillScreen extends AppCompatActivity {
     ImageButton home, bills, profile, add;
+    EditText billname, category, amount, duedate;
+    Button createbill;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,11 @@ public class AddBillScreen extends AppCompatActivity {
         bills = findViewById(R.id.billButton);
         profile = findViewById(R.id.profileButton);
         add = findViewById(R.id.addButton);
+        billname = findViewById(R.id.billName);
+        category = findViewById(R.id.Category);
+        amount = findViewById(R.id.Amount);
+        duedate = findViewById(R.id.dueDate);
+        createbill = findViewById(R.id.createBillButton);
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +55,24 @@ public class AddBillScreen extends AppCompatActivity {
             }
         });
 
+
+        duedate.setOnClickListener(v ->{
+            Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day  = calendar.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    (view, selectedYear, selectedMonth, selectedDay) -> {
+                        String selectedDate = (selectedMonth + 1) + "/" +  selectedDay + "/" + selectedYear;
+                        duedate.setText(selectedDate);
+                    }, year, month, day);
+
+            datePickerDialog.show();
+        });
+
         add.setImageResource(R.drawable.baseline_add_circle_24);
+
+
 
     }
 }
