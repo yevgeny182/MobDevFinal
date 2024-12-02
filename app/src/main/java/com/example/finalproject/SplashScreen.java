1fae5c11796097c084657b90aa544de561663d96
Mike Pino <kurtmikepino@192.168.1.4>
2024-12-03 01:37:58 +0800

    Revised the login pages.

---
+++ app/src/main/java/com/example/finalproject/SplashScreen.java
@@ -0,0 +1,49 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_SCREEN = 5000;
+
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView title;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+
+        logo.setAnimation(topAnim);
+
+        title.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LandingScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
